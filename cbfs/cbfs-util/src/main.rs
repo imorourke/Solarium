1b2024f658a1669e192e7841352160cbe17a9fa6
Ian O'Rourke <cessna.ian@gmail.com>
2026-05-26 18:41:08 -0500

    Add import/export functions into cbfs

--- cbfs/cbfs-util/src/main.rs
+++ cbfs/cbfs-util/src/main.rs
@@ -3,7 +3,7 @@ use cbfs_lib::{
     save_container,
 };
 use clap::{Parser, Subcommand};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 #[derive(Debug, Parser)]
 #[command(version, about)]
@@ -18,6 +18,8 @@ enum CommandOptions {
     Modify(ModifyOptions),
     List(ListOptions),
     Info(InfoOptions),
+    Extract(ExtractOptions),
+    Archive(ArchiveOptions),
 }
 
 #[derive(Debug, Parser)]
@@ -73,6 +75,40 @@ struct ListOptions {
 struct InfoOptions {
     #[arg(help = "image file to read")]
     file: PathBuf,
+}
+
+#[derive(Debug, Parser)]
+struct ExtractOptions {
+    #[arg(help = "cbfs file to extract")]
+    file: PathBuf,
+    #[arg(help = "output directory to extract to")]
+    output: PathBuf,
+}
+
+#[derive(Debug, Parser)]
+struct ArchiveOptions {
+    #[arg(help = "input directory to use as the archive template")]
+    input: PathBuf,
+    #[arg(help = "cbfs file to create")]
+    file: PathBuf,
+    #[arg(short = 'g', long = "gz", help = "enable gzip compression")]
+    gzip: bool,
+    #[arg(short = 'c', long = "sparse", help = "enable sparse file support")]
+    sparse: bool,
+    #[arg(
+        short = 's',
+        long = "secsize",
+        help = "sector size in bytes",
+        default_value_t = 256
+    )]
+    secsize: u16,
+    #[arg(
+        short = 'S',
+        long = "seccount",
+        help = "sector count",
+        default_value_t = 32768
+    )]
+    seccount: u16,
 }
 
 fn main() {
@@ -180,6 +216,102 @@ fn main() {
                 filesystem.sector_count()
             );
             println!("  {} root entries", filesystem.num_primary_entries(),)
+        }
+        CommandOptions::Extract(opt) => {
+            if !opt.output.exists() {
+                std::fs::create_dir(&opt.output).expect("unable to create output directory");
+            } else if opt.output.is_file() {
+                panic!("base directory must be a directory entry");
+            } else if opt.output.is_dir() {
+                std::fs::remove_dir_all(&opt.output).expect("unable to delete output directory");
+                std::fs::create_dir(&opt.output).expect("unable to create output directory");
+            }
+
+            let (hdr, fs) = cbfs_lib::open_container(&opt.file).expect("unable to open filesystem");
+            hdr.check_data()
+                .expect("inconsistent CBFS container header");
+
+            fn write_entries(
+                fs: &FileSystem,
+                current_path: &Path,
+                current_directory: u16,
+            ) -> Result<(), cbfs_lib::FileSystemError> {
+                for n in fs.directory_listing(current_directory)? {
+                    let path = current_path.join(n.get_name());
+                    match n.get_entry_type() {
+                        EntryType::Directory => {
+                            std::fs::create_dir(&path).expect("unable to create directory");
+                            write_entries(fs, &path, n.base_block.get())?;
+                        }
+                        EntryType::File => {
+                            let (_, data) = fs.entry_data(n.base_block.get())?;
+                            std::fs::write(&path, data).expect("unable to write data for file");
+                        }
+                        _ => panic!("unexpected entry data type"),
+                    }
+                }
+
+                Ok(())
+            }
+
+            write_entries(&fs, &opt.output, fs.root_sector())
+                .expect("unable to extract root folder");
+        }
+        CommandOptions::Archive(opt) => {
+            if !opt.input.exists() || !opt.input.is_dir() {
+                panic!("input directory must exist");
+            }
+
+            let mut fs = cbfs_lib::FileSystem::new("cbfs", opt.secsize, opt.seccount)
+                .expect("unable to create filesystem");
+
+            fn read_fs_entries(
+                fs: &mut FileSystem,
+                current_path: &Path,
+                current_directory: u16,
+            ) -> Result<(), cbfs_lib::FileSystemError> {
+                for n in current_path
+                    .read_dir()
+                    .expect("unable to get directory listing")
+                {
+                    let entry = n.expect("unable to get directory entry");
+                    let name = entry
+                        .file_name()
+                        .as_os_str()
+                        .to_str()
+                        .expect("unable to get entry name")
+                        .to_string();
+                    let path = current_path.join(&name);
+
+                    if entry.path().is_dir() {
+                        let new_block = fs
+                            .create_entry(current_directory, &name, EntryType::Directory, &[])
+                            .unwrap();
+                        read_fs_entries(fs, &path, new_block)?;
+                    } else if entry.path().is_file() {
+                        let data = std::fs::read(path).expect("unable to read file data");
+                        fs.create_entry(current_directory, &name, EntryType::File, &data)
+                            .unwrap();
+                    } else {
+                        panic!("unexpected entry data type");
+                    }
+                }
+
+                Ok(())
+            }
+
+            let root = fs.root_sector();
+            read_fs_entries(&mut fs, &opt.input, root).expect("unable to create fs");
+
+            cbfs_lib::save_container(
+                &ContainerHeader::new(CbContainerOptions {
+                    compressed: opt.gzip,
+                    sparse: opt.sparse,
+                }),
+                &fs,
+                &opt.file,
+            )
+            .unwrap();
         }
     }
 }
