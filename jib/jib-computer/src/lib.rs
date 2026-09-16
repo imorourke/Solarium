32b47bc5b6c8ce0b579f378b99641e9eb2dcf8d2
Ian O'Rourke <ianmorou@gmail.com>
2026-09-15 19:51:17 -0500

    Minor updates

--- jib/jib-computer/src/lib.rs
+++ jib/jib-computer/src/lib.rs
@@ -249,7 +249,7 @@ impl JibComputer {
         let devices: [Rc<RefCell<dyn ProcessorDevice>>; _] = [
             self.dev_serial_io.clone(),
             Rc::new(RefCell::new(InterruptClockDevice::default())),
-            Rc::new(RefCell::new(RtcClockDevice)),
+            Rc::new(RefCell::new(RtcClockDevice::default())),
             #[cfg(not(target_arch = "wasm32"))]
             self.dev_rtc_timer.clone(),
             self.dev_pram.clone(),
